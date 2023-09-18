341d6609d3bd283cb59393861cd2569605f676d4
Slavazu <slavi.zubenko@gmail.com>
2023-09-18 22:57:21 +0200

    hw_1 completed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 'use strict';
 
-
-// some code //
+let userName = prompt("What is your name?");
+if (userName !== null && userName !== "") {
+    alert("Hello, " + userName + "! How are you?");
+} else {
+    alert("You haven't entered your name!");
+}
