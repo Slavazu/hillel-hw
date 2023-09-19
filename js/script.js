0f5bf720429a3106f4feff8cf293650a020bf638
Slavazu <slavi.zubenko@gmail.com>
2023-09-20 00:21:28 +0200

    hw_1 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+let admin;
+let name = 'Джон';
+admin = name
+alert(admin)
+
+let planet = 'Earth'
+let visitor = 'userName'
+
+let result = '10' + 10;
+console.log(result);
+console.log(typeof result);
