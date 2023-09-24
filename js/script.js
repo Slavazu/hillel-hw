c492834f5c46c5b8f8c4bf10a4d007f4e8f7c05f
Slavazu <slavi.zubenko@gmail.com>
2023-09-24 22:45:15 +0200

    hw_1 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 'use strict';
+const operand1 = parseFloat(prompt("Введіть перший операнд:"));
+const operand2 = parseFloat(prompt("Введіть другий операнд:"));
+function add(a, b) {
+    const result = a + b;
+    console.log(`result: ${a} + ${b} = ${result}`);
+}function subtract(a, b) {
+    const result = a - b;
+    console.log(`result: ${a} - ${b} = ${result}`);
+}function multiply(a, b) {
+    const result = a * b;
+    console.log(`result: ${a} * ${b} = ${result}`);
+}function divide(a, b) {
+    if (b === 0) {
+        console.log("Error: division by zero is not possible");
+    } else {
+        const result = a / b;
+        console.log(`result: ${a} / ${b} = ${result}`);
+    }
+}
+add(operand1, operand2);
+subtract(operand1, operand2);
+multiply(operand1, operand2);
+divide(operand1, operand2);
