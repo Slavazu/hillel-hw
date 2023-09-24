7119d4d8fd98159fb8395f042e55b3ae03730f3d
Slavazu <slavi.zubenko@gmail.com>
2023-09-24 22:56:26 +0200

    hw_4 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+let operation = prompt("select an operation (add, sub, mult, div):");
+
+let num1 = parseFloat(prompt("enter the first number:"));
+
+let num2 = parseFloat(prompt("enter the second number:"));
+
+let result;
+
+if (operation === "add") {
+    result = num1 + num2;
+} else if (operation === "sub") {
+    result = num1 - num2;
+} else if (operation === "mult") {
+    result = num1 * num2;
+} else if (operation === "div") {
+    if (num2 !== 0) {
+        result = num1 / num2;
+    } else {
+        alert("error: Division by zero!");
+    }
+} else {
+    alert("error: undefined operation!");
+}
+
+
+if (result !== undefined) {
+    alert(num1 + " " + operation + " " + num2 + " = " + result);
+}
