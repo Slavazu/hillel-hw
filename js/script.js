38b5ed403f18f00be9ab20c57411c87b4c0b0f37
Slavazu <slavi.zubenko@gmail.com>
2023-10-06 15:48:16 +0200

    hw_14  completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+let numOrStr = prompt('input number or string?')
+console.log(numOrStr)
+
+switch (true) {
+    case numOrStr === null:
+        console.log('ви скасували');
+        break;
+    case numOrStr.trim() === '':
+        console.log('empty string');
+        break;
+    case isNaN(+numOrStr):
+        console.log('number is Ba_Nan');
+        break;
+    default:
+        console.log('OK!');
+}
+
+
