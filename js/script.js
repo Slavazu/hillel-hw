af3a6d7ef653a58d78647b9c5ffae5950dbe0c86
Slavazu <slavi.zubenko@gmail.com>
2023-10-07 20:00:56 +0200

    hw_13  completed

--- js/script.js
+++ js/script.js
@@ -1,38 +1,51 @@
 'use strict';
 
-// Частина 1
-const birthYear= prompt('Вкажіть свій рік народження?')
-    if (birthYear === null) {
-        alert('Шкода, що ви не захотіли ввести рік народження.');
-    }
-
-const city= prompt('Вкажіть місто у якому Ви живете?')
-    if (city === null) {
-        alert('Шкода, що ви не захотіли ввести місто у якому живете.');
-    }
-
-const sport= prompt('Вкажіть Ваш улюблений вид спорту?')
-    if(sport === null) {
-        alert('Шкода, що ви не захотіли ввести свій улюблений вид спорту.');
-    }
-
-let currentYear= new Date().getFullYear();
-
-let age= currentYear - birthYear;
-
-let country= null;
-if (city === 'Київ' || city === 'Вашингтон' || city === 'Лондон') {
-    if (city === 'Київ') {
-        country = 'України';
-    } else if (city === 'Вашингтон') {
-        country = 'США';
-    } else if (city === 'Лондон') {
-        country = 'Великої Британії';
-    }
-    alert('Вам  '+ age +' років та Ви живете у столиці '+ country +'.');
+const birthYear = prompt('Введіть Ваш рік народження:');
+if (birthYear === null || birthYear === '') {
+    alert('Шкода, що Ви не захотіли ввести свій рік народження.');
 } else {
-    alert('Вам '+ age +' років та Ви живете у місті '+ city +'.');
+    const city = prompt('Введіть назву міста, у якому Ви живете:');
+    if (city === null || city === '') {
+        alert('Шкода, що Ви не захотіли ввести своє місто.');
+    } else {
+        const favoriteSport = prompt('Введіть Ваш улюблений вид спорту:');
+        if (favoriteSport === null || favoriteSport === '') {
+            alert('Шкода, що Ви не захотіли ввести свій улюблений вид спорту.');
+        } else {
+            let champion = '';
+            if (favoriteSport === 'Бокс') {
+                champion = 'Олександр Усик';
+            } else if (favoriteSport === 'Теніс') {
+                champion = 'Роджер Федерер';
+            } else if (favoriteSport === 'Гольф') {
+                champion = 'Тайгер Вудс';
+            }
+            alert('Круто! Хочете стати як ' + champion + ' ?');
+
+            let country = '';
+            if (city === 'Київ') {
+                country = 'України';
+            } else if (city === 'Вашингтон') {
+                country = 'США';
+            } else if (city === 'Лондон') {
+                country = 'Великої Британії';
+            }
+
+            let currentYear = new Date().getFullYear();
+
+            let age = currentYear - birthYear;
+
+            let message = 'Вам ' + age + ' років ';
+            if (country !== '') {
+                message += 'та Ви живете у столиці ' + country + '.';
+            } else {
+                message += 'та Ви живете у місті ' + city + '.';
+            }
+            alert(message);
+        }
     }
+}
+
 
 
 
