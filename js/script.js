c20a0786166b475a46a5a900624332c51af48111
Slavazu <slavi.zubenko@gmail.com>
2023-10-07 18:55:04 +0200

    hw_15  completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,63 @@
 'use strict';
 
+let a = +'';
+let b = +'';
+
+//let result = (a === 0) ? 'Вірно' : '  Невірно';
+//console.log(result);
+
+//let result = (a > 0) ? 'Вірно' : 'Невірно';
+//console.log(result);
+
+//let result = (a < 0) ? 'Вірно' : 'Невірно';
+//console.log(result);
+
+//let result = (a >= 0) ? 'Вірно' : 'Невірно';
+//console.log(result);
+
+//let result = (a <= 0) ? 'Вірно' : 'Невірно';
+//console.log(result);
+
+//let result = (a !== 0) ? 'Вірно' : 'Невірно';
+//console.log(result);
+
+//let result = (a === 'test') ? 'Вірно' : 'Невірно';
+//console.log(result);
+
+//let result = (a === '1') ? 'Вірно' : 'Невірно';
+//console.log(result);
+
+//let result = (0 < a && a < 5) ? 'Вірно' : 'Невірно';
+//console.log(result);
+
+//let result = (a === 0 || a === 2) ? a + 7 : a / 10;
+//console.log(result);
+
+//let result = (a <= 1 && b >= 3) ? a + b : a - b;
+//console.log(result);
+
+//let result = ((a > 2 && a < 11) || (b >= 6 && b < 14)) ? 'Вірно' : 'Невірно';
+//console.log(result);
+
+
+let num = '';
+let result;
+/*
+switch (num) {
+    case 1:
+        result = 'зима';
+        break;
+    case 2:
+        result = 'весна'
+        break;
+    case 3:
+        result = 'літо'
+        break;
+    case 4:
+        result = 'осінь'
+        break;
+    default:
+        result = 'невизначено сезон'
+}
+console.log(result)
+ */
