ece3757bf6394407a801f1512ad83f1eafc9c5b6
Slavazu <slavi.zubenko@gmail.com>
2023-10-09 15:46:25 +0200

    hw_17 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,101 @@
 'use strict';
+
+// let str = '';
+// for (let i = 10; i <= 20; i++) {
+//     str = str + i;
+// }
+// console.log(str);
+
+
+// for (let i = 10; i <= 20; i++) {
+//     let square = i * i;
+//     console.log(square);
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     let mult = i * 7;
+//     console.log(`7 * ${i} = ${mult}`);
+// }
+
+
+// let sum = 0;
+// for (let i = 1; i <= 15; i++) {
+//     sum += i;
+//     console.log(sum);
+// }
+
+
+// let mult = 1;
+// for (let i = 15; i <= 35; i++) {
+//     mult *= i;
+//     console.log(mult);
+// }
+
+
+// let sum = 0;
+// for (let i = 1; i <= 500; i++) {
+//     sum += i;
+// }
+// console.log(sum);
+//
+// const average = sum / (500 - 1 + 1);
+// console.log(average);
+
+
+// let sum = 0;
+// for (let i = 30; i <= 80; i += 2) {
+//     sum += i;
+// }
+// console.log(sum);
+
+
+// for (let i = 100; i <= 200; i++) {
+//     if (i % 3 === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// const num = 77;
+// for (let i = 1; i <= 77; i++) {
+//     if (num % i === 0) {
+//         console.log(i);
+//     }
+// }
+
+// let num2 = 0;
+// for (let i = 1; i <= 77; i++) {
+//     if (num % i === 0 && i % 2 === 0) {
+//         console.log(i);
+//         num2++;
+//     }
+// }
+// console.log(num2)
+
+// let countEvenDivisors = 0;
+// for (let i = 1; i <= num; i++) {
+//     if (num % i === 0 && i % 2 === 0) {
+//         console.log(i);
+//         countEvenDivisors++;
+//     }
+// }
+// console.log(countEvenDivisors);
+
+// let sumEvenDivisors = 0;
+// for (let i = 1; i <= num; i++) {
+//     if (num % i === 0 && i % 2 === 0) {
+//         console.log(i);
+//         sumEvenDivisors += i;
+//     }
+// }
+// console.log(sumEvenDivisors);
+
+
+// for (let i = 1; i <= 10; i++) {
+//     for (let o = 1; o <= 10; o++) {
+//         console.log(i + ' x ' + o + ' = ' + (i * o));
+//     }
+// }
+
+
