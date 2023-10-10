2809c912b323b654bcb0c4869f7fd9e902cf5f32
Slavazu <slavi.zubenko@gmail.com>
2023-10-10 02:18:19 +0200

    hw_18 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,57 @@
 'use strict';
+
+// for (let i = 20; i <= 30; i += 0.5) {
+//     console.log(i);
+// }
+
+
+// for (let dollars = 10; dollars <= 100; dollars += 10) {
+//     const hrn = dollars * 27;
+//     console.log(`${dollars} доларів = ${hrn} гривень`);
+// }
+
+
+// const N = 77;
+// for (let i = 1; i <= 100; i++) {
+//     const square = i * i;
+//     if (square <= N) {
+//         console.log(i);
+//     } else {
+//         break;
+//     }
+// }
+
+
+// let num = 77;
+// let primeNum = true;
+// if (num <= 1) {
+//     primeNum = false;
+// } else if (num > 1) {
+//     for (let i = 2; i < num; i++) {
+//         if (num % i === 0) {
+//             primeNum = false;
+//             break;
+//         }
+//     }
+// }
+// if (primeNum) {
+//     console.log(num + " просте");
+// } else {
+//     console.log(num + " не просте");
+// }
+
+
+// let num = 77;
+// let base = 3;
+// let result = 1;
+// while (result < num) {
+//     result *= base;
+// }
+// if (result === num) {
+//     console.log(num + ' можна отримати ');
+// } else {
+//     console.log(num + ' не можна отримати ');
+// }
+
+
+
