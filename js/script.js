ef46738ea73eddb855a580d3f900d0767c965e70
Slavazu <slavi.zubenko@gmail.com>
2023-10-15 22:16:58 +0200

    hw_21 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+let num = +prompt("Введіть число:");
+
+let degree = +prompt("Введіть сутпінь:");
+
+function exponent(num, degree) {
+    if (isNaN(num) || isNaN(degree)) {
+        return 'Виникла помилка.';
+    }
+
+    return Math.pow(num, degree);
+}
+
+let result = exponent(num, degree);
+
+if (typeof result === 'number') {
+    alert('Результат: ' + result);
+} else {
+    alert(result);
+}
