5ca559f130870dbfe0f7b42291a22b986426b19d
Slavazu <slavi.zubenko@gmail.com>
2023-10-17 01:54:18 +0200

    hw_22 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-'use strict';
+function padString(str, length, char = ' ', leftToRight = true) {
+  if (typeof str !== 'string' || typeof length !== 'number' || typeof char !== 'string' || typeof leftToRight !== 'boolean') {
+    return 'some error';
+  }
+
+  if (str.length >= length) {
+    return str.substr(0, length);
+  }
+
+  const padding = char.repeat(length - str.length);
+  return leftToRight ? padding + str : str + padding;
+}
+
+console.log(padString('hello', 8, '*'));
+console.log(padString('hello', 6, '*', false));
+console.log(padString('hello', 2));
+console.log(padString('hello', 5, '*'));
+console.log(padString('hello', 10, '-', false));
+console.log(padString('hello', 3, '*', false));
+console.log(padString('hello', 10, '!', true));
+console.log(padString('hello', 0, '*'));
+console.log(padString('hello', 8));
