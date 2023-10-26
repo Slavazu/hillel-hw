f5191219fb611b60fb9ce8a2ac73421091b76556
Slavazu <slavi.zubenko@gmail.com>
2023-10-26 19:36:29 +0200

    hw_25 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-'use strict';
+const arrLength = +prompt('Введіть довжину:')
+const userArr = []
+
+for (let i = 0; i < arrLength; i += 1) {
+    const element = +prompt(`Введіть елемент${i}:`)
+    userArr.push(element)
+}
+
+console.log(userArr)
+
+userArr.sort((a, b) => a - b)
+
+console.log(userArr)
+
+userArr.splice(1, 3)
+
+console.log(userArr)
+
+document.write(userArr)
