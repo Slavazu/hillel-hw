24c3c9fad1631c0e286f91b720c5a557cdce19d9
Slavazu <slavi.zubenko@gmail.com>
2023-10-29 20:36:49 +0100

    some changes in hw_26

--- js/script.js
+++ js/script.js
@@ -3,126 +3,58 @@ const array = [
 ];
 
 // TASK #1
-let sumPositiveNum = 0;
-let qtyPositivesNum = 0;
-
-for (let i = 0; i < array.length; i += 1) {
-  if (array[i] > 0) {
-    sumPositiveNum += array[i];
-    qtyPositivesNum += 1;
-  }
-}
+const sumPositiveNum = array.filter((num) => num > 0).reduce((sum, num) => sum + num, 0);
+const qtyPositivesNum = array.filter((num) => num > 0).length;
 
 console.log('Сума позитивних елементів:', sumPositiveNum);
 console.log('Кількість позитивніх елементів:', qtyPositivesNum);
 
-// TASK #2
-// let minElement = array[0];
-// let indexMinElement = 0;
-//
-// for (let i = 1; i < array.length; i += 1) {
-//   if (array[i] < minElement) {
-//     minElement = array[i];
-//     indexMinElement = i;
-//   }
-// }
+// // TASK #2
+// const minElement = Math.min.apply(null, array);
+// const indexMinElement = array.indexOf(minElement);
 //
 // console.log('Найменьший елемент масиву:', minElement);
 // console.log('Порядковий номер найменшого елементу масиву:', indexMinElement);
-
-// TASK #3
-// let maxElement = array[0];
-// let indexMaxElement = 0;
 //
-// for (let i = 1; i < array.length; i += 1) {
-//   if (array[i] > maxElement) {
-//     maxElement = array[i];
-//     indexMaxElement = i;
-//   }
-// }
+// // TASK #3
+// const maxElement = Math.max.apply(null, array);
+// const indexMaxElement = array.indexOf(maxElement);
 //
 // console.log('Найбільший елемент:', maxElement);
 // console.log('Порядковий номер найбільшого елементу:', indexMaxElement);
-
-// TASK #4
-// let qtyNegative = 0;
 //
-// for (let i = 0; i < array.length; i += 1) {
-//   if (array[i] < 0) {
-//     qtyNegative += 1;
-//   }
-// }
+// // TASK #4
+// const qtyNegative = array.filter(num => num < 0).length;
 //
-// console.log('Кфлькість негативних елементів:', qtyNegative);
-
-// TASK #5
-// let qtyOddPositive = 0;
+// console.log('Кількість негативних елементів:', qtyNegative);
 //
-// for (let i = 0; i < array.length; i += 1) {
-//   if (array[i] > 0 && array[i] % 2 !== 0) {
-//     qtyOddPositive += 1;
-//   }
-// }
+// // TASK #5
+// const qtyOddPositive = array.filter(num => num > 0 && num % 2 !== 0).length;
 //
 // console.log('Кількість непарних позитивних елементів:', qtyOddPositive);
-
-// TASK #6
-// let qtyEvenPositive = 0;
 //
-// for (let i = 0; i < array.length; i += 1) {
-//   if (array[i] > 0 && array[i] % 2 === 0) {
-//     qtyEvenPositive += 1;
-//   }
-// }
+// // TASK #6
+// const qtyEvenPositive = array.filter(num => num > 0 && num % 2 === 0).length;
 //
 // console.log('Кількість парних позитивних елементів:', qtyEvenPositive);
-
-// TASK #7
-// let sumEvenPositive = 0;
 //
-// for (let i = 0; i < array.length; i += 1) {
-//   if (array[i] > 0 && array[i] % 2 === 0) {
-//     sumEvenPositive += array[i];
-//   }
-// }
+// // TASK #7
+// const sumEvenPositive = array.filter(num => num > 0 && num % 2 === 0).reduce((sum, num) => sum + num, 0);
 //
 // console.log('Сума парних позитивних елементів:', sumEvenPositive);
-
-// TASK #8
-// let sumOddPositive = 0;
 //
-// for (let i = 0; i < array.length; i += 1) {
-//   if (array[i] > 0 && array[i] % 2 !== 0) {
-//     sumOddPositive += array[i];
-//   }
-// }
+// // TASK #8
+// const sumOddPositive = array.filter(num => num > 0 && num % 2 !== 0).reduce((sum, num) => sum + num, 0);
 //
 // console.log('Сума непарних позитивних елементів:', sumOddPositive);
-
-// TASK #9
-// let multiplyPositive = 1;
 //
-// for (let i = 0; i < array.length; i += 1) {
-//   if (array[i] > 0) {
-//     multiplyPositive *= array[i];
-//   }
-// }
+// // TASK #9
+// const multiplyPositive = array.filter(num => num > 0).reduce((product, num) => product * num, 1);
 //
 // console.log('Результат множення позитивних елементів:', multiplyPositive);
-
-// TASK #10
-// let maxElement = array[0];
 //
-// for (let i = 1; i < array.length; i += 1) {
-//   if (array[i] > maxElement) {
-//     maxElement = array[i];
-//   }
-// }
+// // TASK #10
+// const maxElement = Math.max.apply(null, array);
+// const resultArray = array.map(num => num === maxElement ? num : 0);
 //
-// for (let i = 0; i < array.length; i += 1) {
-//   if (array[i] !== maxElement) {
-//     array[i] = 0;
-//   }
-// }
-//
-// console.log('Обнуління всіх елементів, крім найбільшого:', array);
+// console.log('Обнуління всіх елементів, крім найбільшого:', resultArray);
