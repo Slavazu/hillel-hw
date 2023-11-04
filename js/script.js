bfa43e2daa30435f431cf10aad6b9bad8e3855c2
Slavazu <slavi.zubenko@gmail.com>
2023-11-04 21:27:39 +0100

    hw_29 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-'use strict';
+// Task №1
+const factorial = (n) => {
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+};
+console.log(factorial(3));
+
+// Task №2
+// function pow(num, degree) {
+//   if (degree === 0) {
+//     return 1;
+//   } if (degree > 0) {
+//     return num * pow(num, degree - 1);
+//   }
+//   return 1 / (num * pow(num, -degree));
+// }
+// const result = pow(12, 6);
+// console.log(result);
+
+// Task №3
+// function recursSum(a, b) {
+//   if (b === 0) {
+//     return a;
+//   }
+//   return recursSum(a + 1, b - 1);
+// }
+//
+// const result = recursSum(17, 17);
+// console.log(result);
