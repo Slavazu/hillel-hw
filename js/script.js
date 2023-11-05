b6dc03f916eb2764bd2103d8582f8955ccbce151
Slavazu <slavi.zubenko@gmail.com>
2023-11-05 23:23:41 +0100

    hw_30 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-'use strict';
+const arr = [
+  1, 2, 3, 4, {},
+  [1, 2, 3],
+  [1, 2, 3],
+  [3, 4,
+    [5, 6, 7],
+  ],
+];
+
+function outerFlat() {
+  const res = [];
+
+  function flat(arr) {
+    for (let i = 0; i < arr.length; i += 1) {
+      if (Array.isArray(arr[i])) {
+        flat(arr[i]);
+      } else {
+        res.push(arr[i]);
+      }
+    }
+  }
+
+  flat(arr);
+  return res;
+}
+
+const flat = outerFlat();
+console.log(flat);
