67abe3868bb5cf3b6fb4ce576d9c5f6003b4c2a5
Slavazu <slavi.zubenko@gmail.com>
2023-11-15 21:39:00 +0100

    hw_32 corrected - last chance

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
-const person = { name: 'Слава' };
-
 function myBind(fn, context, ...rest) {
   return function (...args) {
+    const combinedArgs = [...rest, ...args];
     const uniqId = Date.now().toString();
     context[uniqId] = fn;
-    const result = context[uniqId](...args);
+    const result = context[uniqId](...combinedArgs);
     delete context[uniqId];
     return result;
   };
 }
 
-myBind(function (greeting) {
-  console.log(`${greeting}, ${this.name}!`);
-}, person)('Привет');
+const person = {
+  name: 'Cлава',
+  greet(greeting) {
+    console.log(`${greeting}, ${this.name}!`);
+  },
+};
+
+const boundGreet = myBind(person.greet, person, 'Привет');
+boundGreet();
