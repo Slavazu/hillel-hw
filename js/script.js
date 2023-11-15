b0a64f31ffa329fc4ca7b9e366f04f1346f46d8d
Slavazu <slavi.zubenko@gmail.com>
2023-11-15 19:10:55 +0100

    hw_32 corrected - last chance

--- js/script.js
+++ js/script.js
@@ -1,19 +1,15 @@
-const person = {
-  name: 'Слава',
-};
-function info(phone, email) {
-  console.log(`Имя: ${this.name}, Тел.: ${phone}, Email: ${email}`);
-}
+const person = { name: 'Слава' };
+
 function myBind(fn, context, ...rest) {
   return function (...args) {
     const uniqId = Date.now().toString();
     context[uniqId] = fn;
-    const result = context[uniqId](...rest.concat(args));
+    const result = context[uniqId](...args);
     delete context[uniqId];
     return result;
   };
 }
 
-myBind(info, person)('099999222', 'vi@gmail.com');
-myBind(info, person, '099999222')('vi@gmail.com');
-myBind(info, person, '099999222', 'vi@gmail.com')();
+myBind(function (greeting) {
+  console.log(`${greeting}, ${this.name}!`);
+}, person)('Привет');
