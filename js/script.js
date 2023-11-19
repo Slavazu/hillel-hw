2a5cf81a103c461e63428e4be4db6859ee369f1d
Slavazu <slavi.zubenko@gmail.com>
2023-11-19 23:41:49 +0100

    hw_38 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-'use strict';
+function createTable() {
+  const tableContainer = document.createElement('div');
+  const table = document.createElement('table');
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < 10; i += 1) {
+    const row = document.createElement('tr');
+
+    for (let j = 0; j < 10; j += 1) {
+      const cell = document.createElement('td');
+      const cellValue = i * 10 + j + 1;
+      cell.textContent = cellValue;
+      row.appendChild(cell);
+    }
+    fragment.appendChild(row);
+  }
+
+  table.appendChild(fragment);
+  tableContainer.appendChild(table);
+  document.body.appendChild(tableContainer);
+}
+
+createTable();
