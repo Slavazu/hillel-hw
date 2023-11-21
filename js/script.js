4b313f5d02ace522c7f77220eb6eee147ff19df1
Slavazu <slavi.zubenko@gmail.com>
2023-11-21 17:01:12 +0100

    hw_37 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-'use strict';
+function generateList(array) {
+  const ul = document.createElement('ul');
+
+  for (const item of array) {
+    const li = document.createElement('li');
+
+    if (Array.isArray(item)) {
+      li.appendChild(generateList(item));
+    } else {
+      li.textContent = item;
+    }
+
+    ul.appendChild(li);
+  }
+
+  return ul;
+}
+
+const myArray = [1, 2, [1.1, 1.2, 1.3], 3];
+const myList = generateList(myArray);
+document.body.appendChild(myList);
