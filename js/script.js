8d0b0d01a0d63af05a8dc191af6673632ff31646
Slavazu <slavi.zubenko@gmail.com>
2023-11-23 16:39:15 +0100

    hw_34 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-'use strict';
+function insertWordInto(initialString) {
+  return function (newWord) {
+    const words = initialString.split(' ');
+    const randomIndex = Math.floor(Math.random() * (words.length + 1));
+
+    words.splice(randomIndex, 0, newWord);
+
+    const resultString = words.join(' ');
+
+    return resultString;
+  };
+}
+
+const insert = insertWordInto('hello world');
+const result = insert('Odesa');
+console.log(result);
+const secondResult = insert('Odesa');
+console.log(secondResult);
+const thirdResult = insert('Odesa');
+console.log(thirdResult);
+const fourthResult = insert('Odesa');
+console.log(fourthResult);
