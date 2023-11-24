6e1dbc1b47b7bcf85afec5d3a16d5bae4e099c1f
Slavazu <slavi.zubenko@gmail.com>
2023-11-25 00:02:46 +0100

    hw_40 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-'use strict';
+const folderPath = './images/';
+const fileNames = ['1.webp', '2.webp', '3.webp', '4.webp', '5.webp', '6.webp', '7.webp', '8.webp', '9.webp'];
+const randomFileName = fileNames[Math.floor(Math.random() * fileNames.length)];
+const randomImagePath = `${folderPath}/${randomFileName}`;
+const randomImageElement = document.querySelector('#randomImage');
+randomImageElement.src = randomImagePath;
+console.log(randomImagePath);
