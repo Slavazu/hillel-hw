3bcdc95af9c0806197f3fa451c498a57ba763a9e
Slavazu <slavi.zubenko@gmail.com>
2023-11-25 00:14:40 +0100

    hw_41 completed task-2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 (function () {
   document.querySelector('.tab').classList.add('active');
   document.querySelector('.tab-panel').classList.add('active');
+
   function selectPanel(e) {
     const { target } = e.target.dataset;
     document.querySelectorAll('.tab, .tab-panel').forEach((el) => el.classList.remove('active'));
     e.target.classList.add('active');
     document.querySelector(`.${target}`).classList.add('active');
   }
-  document.querySelectorAll('.tab').forEach((el) => {
-    el.addEventListener('click', selectPanel);
-  });
+
+  document.querySelectorAll('.tab').forEach((el) => el.addEventListener('click', selectPanel));
 }());
