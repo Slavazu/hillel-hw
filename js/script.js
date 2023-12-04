83e894dcf43b9eabba26f43f4cab477acfe9077a
Slavazu <slavi.zubenko@gmail.com>
2023-12-05 00:23:19 +0100

    hw_42 completed

--- js/script.js
+++ js/script.js
@@ -1,44 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('form');
+  const form = document.getElementById('Form');
 
-  if (form) {
-    form.addEventListener('submit', (event) => {
-      event.preventDefault();
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-      const formData = new FormData(form);
-      const formObject = {};
+    const formData = {
+      email: form.elements.email.value,
+      firstName: form.elements.firstName.value,
+      lastName: form.elements.lastName.value,
+      nickname: form.elements.nickname.value,
+      password: form.elements.password.value,
+      option: form.elements.option.value,
+      message: form.elements.message.value,
+      file: form.elements.file.value,
+      terms: form.elements.terms.checked,
+    };
 
-      formData.forEach((value, key) => {
-        formObject[key] = value;
-      });
+    localStorage.setItem('formData', JSON.stringify(formData));
 
-      const jsonData = JSON.stringify(formObject);
-      localStorage.setItem('formData', jsonData);
-    });
-  } else {
-    // Если формы нет (на странице displayData.html)
-    const userDataList = document.getElementById('userDataList');
-    const jsonData = localStorage.getItem('formData');
-
-    if (jsonData) {
-      try {
-        const formObject = JSON.parse(jsonData);
-
-        for (const key in formObject) {
-          if (formObject.hasOwnProperty(key)) {
-            const listItem = document.createElement('li');
-            listItem.className = 'list-group-item';
-            listItem.innerHTML = `<strong>${key}:</strong> ${formObject[key]}`;
-            userDataList.appendChild(listItem);
-          }
-        }
-      } catch (error) {
-        console.error('Error parsing JSON:', error);
-      }
-    } else {
-      const noDataMessage = document.createElement('p');
-      noDataMessage.textContent = 'No user data available.';
-      userDataList.appendChild(noDataMessage);
-    }
-  }
+    form.reset();
+  });
 });
