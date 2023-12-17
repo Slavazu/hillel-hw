4381ba88a4d2e376ecdbb26f8ad938e6604c86ac
Slavazu <slavi.zubenko@gmail.com>
2023-12-18 00:39:11 +0100

    hw_todo-list

--- js/script.js
+++ js/script.js
@@ -1,32 +1,86 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const button = document.querySelector('[data-add]');
-  const input = document.querySelector('[data-input]');
-  const list = document.querySelector('[data-list]');
+const todoApp = (function () {
+  const todoModel = {
+    todos: [],
 
-  button.addEventListener('click', () => {
-    if (input.value.trim() !== '') {
-      const li = createTodoElement(input.value);
-      list.prepend(li);
-      input.value = '';
-    } else {
-      alert('Введите текст задачи!');
-    }
-  });
+    addTodo(text) {
+      if (text.trim() !== '') {
+        this.todos.unshift({ text });
+        this.saveToLocalStorage();
+      }
+    },
 
-  const createTodoElement = (text) => {
-    const li = document.createElement('li');
-    li.className = 'todo_list_item';
-    li.textContent = text;
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+      this.saveToLocalStorage();
+    },
 
-    const deleteBtn = document.createElement('button');
-    deleteBtn.innerText = 'Delete';
+    saveToLocalStorage() {
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+    },
 
-    deleteBtn.addEventListener('click', () => {
-      li.remove();
-    });
-
-    li.appendChild(deleteBtn);
-
-    return li;
+    loadFromLocalStorage() {
+      const storedTodos = localStorage.getItem('todos');
+      this.todos = storedTodos ? JSON.parse(storedTodos) : [];
+    },
   };
+
+  const todoView = {
+    button: document.querySelector('[data-add]'),
+    input: document.querySelector('[data-input]'),
+    list: document.querySelector('[data-list]'),
+
+    init(controller) {
+      this.button.addEventListener('click', () => {
+        controller.handleAddTodo();
+      });
+    },
+
+    render(todos, removeTodoHandler) {
+      this.list.innerHTML = '';
+      todos.forEach((todo, index) => {
+        const li = document.createElement('li');
+        li.className = 'todo_list_item';
+        li.textContent = todo.text;
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.innerText = 'Delete';
+
+        deleteBtn.addEventListener('click', () => {
+          removeTodoHandler(index);
+        });
+
+        li.appendChild(deleteBtn);
+        this.list.appendChild(li);
+      });
+    },
+  };
+
+  const todoController = {
+    model: todoModel,
+    view: todoView,
+
+    init() {
+      this.model.loadFromLocalStorage();
+      this.view.init(this);
+      this.view.render(this.model.todos, this.handleRemoveTodo.bind(this));
+    },
+
+    handleAddTodo() {
+      const text = this.view.input.value;
+      this.model.addTodo(text);
+      this.view.render(this.model.todos, this.handleRemoveTodo.bind(this));
+      this.view.input.value = '';
+    },
+
+    handleRemoveTodo(index) {
+      this.model.removeTodo(index);
+      this.view.render(this.model.todos, this.handleRemoveTodo.bind(this));
+    },
+  };
+
+  return todoController;
+}());
+
+document.addEventListener('DOMContentLoaded', () => {
+  todoApp.init();
 });
